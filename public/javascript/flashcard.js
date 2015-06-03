8ce63c628007b9dfe01279e1b122f262631c3647
Honey Silvas <honeysilvas@gmail.com>
2015-06-03 13:25:55 -0400

    Disabled scroll bars.
    Disabled scroll bars.

--- public/javascript/flashcard.js
+++ public/javascript/flashcard.js
@@ -5,23 +5,31 @@
 *
 */
 
+	function hide_scroll_bars() {
+		document.documentElement.style.overflow = "hidden";  // For Mozilla Firefox and Google Chrome.
+		document.body.scroll = "no"; // For Internet Explorer.
+	}
+
 	function initialize_card(){
+		hide_scroll_bars();
+		
+		card_counter = 1;
+		preload_next_card( card_counter + 1 );
+		
 		card_toggle = "text";
 		
 		card_text 		= document.getElementById( "card_text" );
 		card_image 		= document.getElementById( "card_image" );
 		card_audio 		= document.getElementById( "card_audio" );
 		preload_audio 	= document.getElementById( "preload_audio" );
-		card_text.style.visibility = "visible";
+		card_text.style.visibility = "visible";	
 		
-		card_counter = 1;
-		preload_next_card();
 		play_card();
 	}
 
-	function preload_next_card(){
-		document.images[ "preload_image" ].src = card_directory + card_array[ 1 ][ card_counter + 1 ];
-		preload_audio.setAttribute( "src", card_directory + card_array[ 2 ][ card_counter + 1 ] );		
+	function preload_next_card( card ){
+		document.images[ "preload_image" ].src = card_directory + card_array[ 1 ][ card ];
+		preload_audio.setAttribute( "src", card_directory + card_array[ 2 ][ card ] );				
 	}
 
 	function play_card(){
@@ -30,15 +38,16 @@
 			
 			card_text.innerHTML = card_array[ 0 ][ card_counter ];	
 			card_audio.setAttribute( "src", card_directory + card_array[ 2 ][ card_counter ] );		
+			document.images[ "card_image" ].src = "../images/spacer.png";
 			
 			card_text.style.visibility = "visible";
 			card_image.style.visibility = "hidden";
-			preload_next_card();
+			preload_next_card( card_counter + 1 );
 			
 		} else {
 		
 			card_toggle = "text";
-			
+
 			document.images[ "card_image" ].src = "../images/spacer.png";
 			document.images[ "card_image" ].src = card_directory + card_array[ 1 ][ card_counter ];
 			
